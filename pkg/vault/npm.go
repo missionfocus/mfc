971e6607e3592d727981dea451126c761a0b2195
Collin Day <ccday@missionfocus.com>
2019-05-28 13:28:27 -0400

    Update NPM authentication command to create per-registry auth entries

--- pkg/vault/npm.go
+++ pkg/vault/npm.go
@@ -1,44 +1,79 @@
 package vault
 
 import (
-	"encoding/base64"
+	"bytes"
 	"github.com/hashicorp/vault/api"
 	"io/ioutil"
+	"os"
+	"path/filepath"
 	"regexp"
 )
 
 type NPMSecret struct {
-	Username string
-	Password string
+	Registry string
+	Token string
 }
 
 func NewNPMSecret(secret *api.Secret) *NPMSecret {
 	return &NPMSecret{
-		Username: secret.Data["username"].(string),
-		Password: secret.Data["password"].(string),
+		Registry: secret.Data["registry"].(string),
+		Token: secret.Data["token"].(string),
 	}
 }
 
-func (s *NPMSecret) Base64() string {
-	return base64.StdEncoding.EncodeToString([]byte(s.Username + ":" + s.Password))
-}
-
-var authRe = regexp.MustCompile(`(?m)^_auth=[0-9A-z+/=]+$`)
+var authRe = regexp.MustCompile(`(?m)^//(.+):_authToken=(.+)$`)
 
 func (s *NPMSecret) UpdateNpmrc(path string) error {
-	content, err := ioutil.ReadFile(path)
+	f, err := openOrCreate(path, 0700, 0600)
 	if err != nil {
 		return err
 	}
-
-	authStr := "_auth=" + s.Base64()
-	if authRe.Match(content) {
-		content = authRe.ReplaceAll(content, []byte(authStr))
-	} else {
-		if content[len(content)-1] != '\n' {
-			content = append(content, '\n')
-		}
-		content = append(content, []byte(authStr+"\n")...)
+	content, err := ioutil.ReadAll(f)
+	if err != nil {
+		return err
 	}
+	if err := f.Close(); err != nil {
+		return err
+	}
+
+	// First case, the registry already exists
+	for _, match := range authRe.FindAllSubmatch(content, -1) {
+		if !bytes.Equal(match[1], []byte(s.Registry)) {
+			continue
+		}
+
+		updated := bytes.Replace(match[0], match[2], []byte(s.Token), 1)
+		content = bytes.Replace(content, match[0], updated, 1)
+
+		return nil
+	}
+
+	// Other case, it doesn't exist, so append it
+	content = append(content, []byte("\n//" + s.Registry + ":_authToken=" + s.Token)...)
 	return ioutil.WriteFile(path, content, 0600)
+}
+
+func openOrCreate(path string, dirPerm, filePerm os.FileMode) (*os.File, error) {
+	if exists, err := fileExists(path); exists {
+		return os.Open(path)
+	} else if err != nil {
+		return nil, err
+	}
+
+	dir, _ := filepath.Split(path)
+	if err := os.MkdirAll(dir, dirPerm); err != nil {
+		return nil, err
+	}
+
+	return os.OpenFile(path, os.O_RDWR|os.O_CREATE, filePerm)
+}
+
+func fileExists(path string) (bool, error) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return false, nil
+	} else if err != nil {
+		return false, err
+	} else {
+		return true, nil
+	}
 }
