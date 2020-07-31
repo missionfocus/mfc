694101ecaf6e930606ceac115f86d45f72a3a1f8
David Busey <dmbusey@missionfocus.com>
2020-07-31 16:20:31 -0400

    example fix

--- cmd/mfc/bpe.go
+++ cmd/mfc/bpe.go
@@ -17,16 +17,16 @@ var bpeCmd = &cobra.Command{
 	Aliases: []string{"bpe"},
 }
 
-const gitlabVelocityExample = `
-    mfc gitlab check velocity  ""		# Generate documentation for mfc in markdown format
- 	mfc gitlab check velocity "GDAC 18" # Gets the velocity for the GDAC 18 milestone`
+const bpeVelocityExample = `
+    mfc bpe velocity <milestone>		# Generate documentation for mfc in markdown format
+ 	mfc bpe velocity "GDAC 18"  		# Gets the velocity for the GDAC 18 milestone`
 
 //bpeVelocityReportCmd returns the weight a mfm completed during a milestone. TODO iterations once implemented
 var bpeVelocityReportCmd = &cobra.Command{
 	Use:     "velocity",
 	Short:   "Check each MFM velocity for a given milestone and iteration",
 	Args:    cobra.ExactArgs(1),
-	Example: gitlabVelocityExample,
+	Example: bpeVelocityExample,
 	Run: func(cmd *cobra.Command, args []string) {
 		vClient, err := getVaultClientWithToken()
 		check(err)
